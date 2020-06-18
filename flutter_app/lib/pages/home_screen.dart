13392ae56a4bb313836ebe0e99acb61e03558280
TimZInGitOSC <huai_1989@yeah.net>
2020-06-18 18:53:04 +0800

    [dart][UI]

--- flutter_app/lib/pages/home_screen.dart
+++ flutter_app/lib/pages/home_screen.dart
@@ -5,21 +5,8 @@ var selectItValue;
 var selectItemValue;
 
 class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(title: Text('第一页')),
-     //body: Center(child: Text('Home1'),)
-     body: DropdownButton(items: null, onChanged: null),
 
-    );
-  }
-}
-
-
-
-class DropdownButtonDefault extends StatelessWidget {
+  // 生成数据源
   List<DropdownMenuItem> generateItemList() {
     final List<DropdownMenuItem> items =List();
     final DropdownMenuItem item1 = DropdownMenuItem(value: 'RU2009',child: Text('RU2009'));
@@ -32,43 +19,22 @@ class DropdownButtonDefault extends StatelessWidget {
     items.add(item4);
     return items;
   }
+
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      hint: Text('下拉菜单选择要交易的商品'),
-      value: selectItValue,
-      items: generateItemList(),
-      onChanged: (T){},
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      // 修改标题为'第一页！'
+      appBar: AppBar(title: Text('第一页！')),
+     //body: Center(child: Text('Home1'),)
+    //  修改页面UI
+      body: DropdownButton(
+        hint: Text('下拉菜单选择要交易的商品'),
+        value: selectItValue,
+        items: generateItemList(),
+        onChanged: (T){},
+      )
+
     );
   }
 }
-
-/*
-//DropdownButton自定义的实例
-class DropdownButtonCustom extends StatelessWidget{
-  final widget;
-  final parent;
-  const DropdownButtonCustom([this.widget, this.parent]):super();
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton(
-      items: getListData(),
-      hint: Text('下拉选择'),
-      value: selectItemValue,
-      onChanged: (T){
-        if (parent.mounted){
-          parent.setState((){
-            selectItemValue = T;
-          });
-        }
-      },
-      elevation: 24,
-      style: TextStyle(
-        color: Colors.red
-      ),
-      isExpanded: true,
-      iconSize: 50.0,
-    );
-  }
-
-}*/
